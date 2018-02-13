46ff69a2940eb7690c6387f0a7430484e78a83ac
jackzajac7 <azajac1@mail.csuchico.edu>
2018-02-13 11:26:12 -0800

    horoscope

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 {
   //declare variables
   std::string first, last;
+  int lucky;
 
   std::cout<< "Please enter your first and last name." <<std::endl;
 
@@ -18,6 +19,8 @@ int main()
   //tell fortune
 
   std::cout<< "Welcome, " <<first[0]<<"."<<last[0]<<"."<< ", here is your fortune..."  <<std::endl;
+  
+  std::cout<< "Your lucky number is "<<first.length()<<"!"<<std::endl;
 
   return 0;
 }
