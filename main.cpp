8bbc2d5a87423d99e381ab4019852340a92c9d9f
jackzajac7 <azajac1@mail.csuchico.edu>
2018-02-13 11:14:36 -0800

    horoscope

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
-//Authors:
+//Authors: Aero Zajac
 #include<iostream>
 #include<string>
 
-using namespace std;
 
 int main()
 {
   //declare variables
+  std::string first, last;
+
+  std::cout<< "Please enter your first and last name." <<std::endl;
 
   //get user input
+
+  std::cin>>first;
+  std::cin>>last;
 
   //tell fortune
 
